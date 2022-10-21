9092404de6109819db277e0d8678e42238f1fe82
destiowahyu@gmail.com <destiowahyu@gmail.com>
2022-10-22 00:10:24 +0700

    first commit

--- app/src/main/java/com/example/menumakanan/MainActivity.java
+++ app/src/main/java/com/example/menumakanan/MainActivity.java
@@ -25,37 +25,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void initData() {
         this.listMakanan = new ArrayList<>();
-        listMakanan.add(new Kuliner("Bakso",
+        listMakanan.add(new Kuliner("Nasi Goreng",
                 "Rp. 10.000",
-                "Bakso murah porsi banyak, segar untuk di makan saat hujan",
-                R.drawable.bakso));
-        listMakanan.add(new Kuliner("Bubur Ayam",
-                "Rp. 10.000",
-                "Bubur ayam full toping bisa pilih toping sesuka hati",
-                R.drawable.bubur));
-        listMakanan.add(new Kuliner("Lalapan",
-                "Rp. 7.000",
-                "Sayur segar serta fresh untuk pendorong nasi",
-                R.drawable.lalapan));
+                "Nasi goreng enak di nikmatin bersama keluarga",
+                R.drawable.nasigoreng));
         listMakanan.add(new Kuliner("Mie Ayam",
                 "Rp. 12.000",
                 "Mie dengan toping ayam sangat segar dan enak",
                 R.drawable.mieayam));
-        listMakanan.add(new Kuliner("Sate",
-                "Rp. 20.000",
-                "Sate ayam atau kambing dengan bumbu kecap atau bumbu kacang.",
-                R.drawable.sate));
-        listMakanan.add(new Kuliner("Soto Ayam",
-                "Rp. 15.000",
+        listMakanan.add(new Kuliner("Bubur Ayam",
+                "Rp. 10.000",
+                "Bubur ayam full toping bisa pilih toping sesuka hati",
+                R.drawable.bubur));
+        listMakanan.add(new Kuliner("Capcay",
+                "Rp. 7.000",
+                "Sayur segar serta fresh untuk pendorong nasi",
+                R.drawable.capcay));
+        listMakanan.add(new Kuliner("Papeda",
+                "Rp. 2.000",
+                "tekstur yang kenyalll, bikin ketagihan looooo",
+                R.drawable.pappeda));
+        listMakanan.add(new Kuliner("Soto Sapi",
+                "Rp. 10.000",
                 "Soto dengan suwiran ayam dan telur bacem.",
-                R.drawable.soto));
+                R.drawable.sotosapi));
         listMakanan.add(new Kuliner("Seblak",
                 "Rp. 15.000",
                 "Seblak dengan toping kerupuk telur dan ayam",
                 R.drawable.seblak));
-        listMakanan.add(new Kuliner("Nasi Padang",
-                "Rp. 20.000",
-                "Makanan yg paling enak di makan menggunakan tangan",
-                R.drawable.nasipadang));
+        listMakanan.add(new Kuliner("Tahu Gimbal",
+                "Rp. 15.000",
+                "Makanan Kesukaan Elfira",
+                R.drawable.tahugimbal));
     }
 }
